be2ec436c6de15f6f03d848aa8e7b9e120f50b5c
Alex M <alexgotfrag@gmail.com>
2017-03-17 20:43:32 +0300

    Top down view

--- Assets/Scripts/ViewChanger.cs
+++ Assets/Scripts/ViewChanger.cs
@@ -4,20 +4,7 @@ using UnityEngine;
 
 public class ViewChanger : MonoBehaviour {
 
-    public GameObject topCamera;
-    public GameObject fpsCamera;
-    public GameObject airplaneCamera;
-
     public List<GameObject> modeList = new List<GameObject>();
-
-	/// <summary>
-    /// Add views set through inspector to the modes list
-    /// </summary>
-	void Start () {
-        modeList.Add(topCamera);
-        modeList.Add(fpsCamera);
-        modeList.Add(airplaneCamera);
-    }
 
     /// <summary>
     /// Disables every viewm then enables the one passed in the argument
